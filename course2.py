e6fba5f9e5267d927a2f45f0abb2834e716cefa1
Sushil Padhi <skp282018@gmail.com>
2022-05-10 23:30:22 +0530

    adding new cours2

---
+++ course2.py
@@ -0,0 +1,9 @@
+
+print("Adding course 2 answers to Course 2 exercise")
+
+"Sushil Padhi" \
+"I Like Algoritm Analysis" \
+"Algorithm is a step-by-step procedure, " \
+"which defines a set of instructions to be executed in a certain order to get the desired output. " \
+"Algorithms are generally created independent of underlying languages, " \
+"i.e. an algorithm can be implemented in more than one programming language."
